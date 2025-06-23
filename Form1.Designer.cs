c5ea8131734394c6e4badb65c1a593eec2351bd9
tang316 <rextang0316@gmail.com>
2025-06-23 18:01:24 +0800

    修改屬性
    修改屬性

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,33 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnGo
             // 
-            this.button1.Location = new System.Drawing.Point(483, 189);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnGo.Location = new System.Drawing.Point(483, 189);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(127, 53);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "開始";
+            this.btnGo.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtShow
             // 
-            this.textBox1.Location = new System.Drawing.Point(243, 205);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(194, 25);
-            this.textBox1.TabIndex = 1;
+            this.txtShow.Location = new System.Drawing.Point(243, 205);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(194, 25);
+            this.txtShow.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.btnGo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -64,8 +64,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtShow;
     }
 }
 
